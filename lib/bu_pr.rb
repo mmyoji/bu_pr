e5179463fb2da552bd6b6831bead4b58e75193fc
Masaya Myojin <m.myojin0425@gmail.com>
2017-01-04 23:10:49 +0900

    :put_litter_in_its_place: redundant blank lines

--- lib/bu_pr.rb
+++ lib/bu_pr.rb
@@ -2,11 +2,8 @@
 require "bu_pr/version"
 require "bu_pr/configuration"
 require "bu_pr/git"
-
 require "bu_pr/handlers/github"
-
 require "bu_pr/runner"
-
 require "bu_pr/railtie" if defined?(Rails::Railtie)
 
 module BuPr
